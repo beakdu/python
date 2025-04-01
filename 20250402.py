5f59b2b699f3ad888e304af48c402a80533decce
LeeJungHyun <167539016+beakdu@users.noreply.github.com>
2025-04-02 05:10:29 +0900

    Add files via upload

---
+++ 20250402.py
@@ -0,0 +1,10 @@
+print(int(3.8))
+print(float(3))
+#문자형을 정수형으로//
+print(int("2")+int("3"))
+#문자형을 소수형으로
+print(float("1.1")+float("2.5"))
+#숫자를 문자형으로
+print(str(2)+str(5))
+age = 7
+print("제 나이는 " + str(age) + "살입니다.")
